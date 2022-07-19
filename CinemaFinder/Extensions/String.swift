e9829459446f026e883380c83867b11b54c10a60
pranaygali <pranay.gali@gmail.com>
2022-07-19 00:01:34 -0400

    First Commit

---
+++ CinemaFinder/Extensions/String.swift
@@ -0,0 +1,52 @@
+//
+//  String.swift
+//  FlowerApp
+
+
+import Foundation
+
+
+struct STRING {
+    static let errorPassword = "Please enter password"
+    static let errorValidPassword = "Please enter valid password"
+    static let errorEmail = "Please enter email"
+    static let errorValidEmail = "Please enter valid email"
+    static let errorEnterName = "Please enter name"
+    static let errorMobile = "Please enter mobile number"
+    static let errorValidMobile = "Please enter valid mobile number"
+    static let errorCity = "Please enter address"
+    static let errorConfirmPassword = "Please enter confirm password"
+    static let errorPasswordMismatch = "Password mismatch"
+    static let errorValidCreatePassword = "Password must contain (1upper 1lower 1special 1numeric)"
+    static let errorValidOtp = "Please enter otp"
+    static let errorPasswordCount = "Please enter password with atleast 8 characters"
+    static let errorEnterTheaterName = "Please enter theater name"
+    static let errorEnterTheaterAddress = "Please enter theater's address"
+    static let errorEnterMovieName = "Please enter movie name"
+    static let errorEnterMovieType = "Please enter movie's type"
+    static let errorEnterDName = "Please enter movie's director name"
+    static let errorEnterPName = "Please enter movie's producer's name"
+    static let errorEnterCast = "Please enter movie's cast"
+    static let errorSelectImage = "Please select image"
+}
+
+let cUser       = "users"
+let cUID        = "uid"
+let cName       = "fullName"
+let cEmail      = "email"
+let cPhone      = "contactNumber"
+let cAddress    = "address"
+let cPassword   = "password"
+let cTheater    = "theater"
+let cTID        = "t_id"
+let cTName      = "t_name"
+let cTAddress   = "t_address"
+let cImageURL   = "imageURL"
+let cMovie      = "movie"
+let cMID        = "m_id"
+let cMName      = "m_name"
+let cMCast      = "m_cast"
+let cMGerne     = "m_gerne"
+let cMDName     = "m_director_name"
+let cMPName     = "m_producer_name"
+
